896711542778685808fb6ce858128840bb18d5bc
gab1005 <gabrielqwerty1@gmail.com>
2024-05-06 09:03:32 -0300

    change some files

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import Page1 from "@/view/page-1";
+
+export default function Home() {
+  return (
+    <Page1 />
+  );
+}
---
+++ src/view/page-1.tsx
@@ -0,0 +1,7 @@
+const Page1 = () => {
+    return(
+        <div className="bg-red-300 w-full h-80" >testando a nova pagina</div>
+    );
+}
+
+export default Page1;
